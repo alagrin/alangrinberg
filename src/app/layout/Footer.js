50493f274e5852e8bfb083489ca5d67b0fd13141
alagrin <grinny@icloud.com>
2020-04-24 22:11:55 -0700

    added basic pages, prep for routing, readme added

---
+++ src/app/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <div className="content has-text-centered">
+        <p>
+          Built on <strong>React</strong> with <strong>Bulma</strong> by Me!{" "}
+          <a href="https://github.com/alagrin">Alan Grinberg</a>
+          <br />
+          Copyright 2020
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
