7a8ec99a23ba458b24e87864844020f823c0256a
alagrin <grinny@icloud.com>
2020-05-23 22:00:38 -0700

    for heroku

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "./../Home";
 import Blog from "./../Blog";
 import BlogPost from "../blog/BlogPost";
 import Contact from "./Contact";
+import App from "../../App";
 
 
 const Router = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={App} />
     <Route exact path="/blog" component={Blog} />
     <Route exact path="/contact" component={Contact} />
     <Route path="/blog/:blogPost" component={BlogPost} />
