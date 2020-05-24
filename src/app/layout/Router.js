8286b109f951504df638d27629cacac34805e96e
alagrin <grinny@icloud.com>
2020-05-23 22:31:45 -0700

    minor updates

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -3,12 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import Blog from "./../Blog";
 import BlogPost from "../blog/BlogPost";
 import Contact from "./Contact";
-import App from "../../App";
+import Home from "../Home";
 
 
 const Router = () => (
   <Switch>
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={Home} />
     <Route exact path="/blog" component={Blog} />
     <Route exact path="/contact" component={Contact} />
     <Route path="/blog/:blogPost" component={BlogPost} />
