0e241b9af35ca1604d79ffa1e567bb4a56876e19
alagrin <grinny@icloud.com>
2020-05-23 22:49:09 -0700

    heroku updates

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static(__dirname + "/"));
+app.listen(process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join('build', 'index.html'));
+    });
+  }
